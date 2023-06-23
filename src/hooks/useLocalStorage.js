deedccca64b44d1c3341e0b47554141f5f116833
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-06-23 20:47:05 +0600

    [Done] get setting from localstorage removed

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -5,12 +5,12 @@ import { useState, useEffect } from 'react';
 export default function useLocalStorage(key, defaultValue) {
   const [value, setValue] = useState(defaultValue);
 
-  useEffect(() => {
-    const storedValue = window.localStorage.getItem(key);
-    if (storedValue !== null) {
-      setValue(JSON.parse(storedValue));
-    }
-  }, [key]);
+  // useEffect(() => {
+  //   const storedValue = window.localStorage.getItem(key);
+  //   if (storedValue !== null) {
+  //     setValue(JSON.parse(storedValue));
+  //   }
+  // }, [key]);
 
   useEffect(() => {
     window.localStorage.setItem(key, JSON.stringify(value));
