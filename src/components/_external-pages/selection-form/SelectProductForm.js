3b956c03bae2bb37bb4c195d0925ee538cf507ad
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-06-24 18:28:41 +0600

    [Done] Select category and product

--- src/components/_external-pages/selection-form/SelectProductForm.js
+++ src/components/_external-pages/selection-form/SelectProductForm.js
@@ -18,87 +18,115 @@ export default function SelectProductForm() {
     };
 
     return (
-            <Box sx={{ maxWidth: '100%' }}>
-                <form >
-                    <MotionInView variants={varFadeInDown}>
-                        <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
-                            <Grid item xs={12} sm={4}>
-                                <Typography variant="h5" mb={2} align="center">
-                                    Select Product
-                                </Typography>
-                                <FormControl fullWidth sx={{ maxWidth: '100%' }}>
-                                    <InputLabel id="product">Select Product</InputLabel>
-                                    <Select
-                                        labelId="product"
-                                        id="product"
-                                        name="product"
-                                        value={formData.product}
-                                        label="Select Product"
-                                        onChange={handleChange}
-                                    >
-                                        <MenuItem value=""> <em>None</em> </MenuItem>
-                                        <MenuItem value="1">Product 1</MenuItem>
-                                        <MenuItem value="2">Product 2</MenuItem>
-                                        <MenuItem value="3">Product 3</MenuItem>
-                                    </Select>
-                                </FormControl>
-                            </Grid>
+        <Box sx={{ maxWidth: '100%' }}>
+            <form >
+                <MotionInView variants={varFadeInDown}>
+                    <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
+                        <Grid item xs={12}>
+                            <Typography variant="h5" mb={2} align="center">
+                                Select Category
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <FormControl fullWidth sx={{ maxWidth: '100%' }}>
+                                <InputLabel id="product">Select Category</InputLabel>
+                                <Select
+                                    labelId="category"
+                                    id="category"
+                                    name="category"
+                                    value={formData.category}
+                                    label="Select Category"
+                                    onChange={handleChange}
+                                >
+                                    <MenuItem value=""> <em>None</em> </MenuItem>
+                                    <MenuItem value="1">Category 1</MenuItem>
+                                    <MenuItem value="2">Category 2</MenuItem>
+                                    <MenuItem value="3">Category 3</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+
+                        {
+                            formData.category && (
+                                <Grid item xs={12} sm={4}>
+                                    {/* <Typography variant="h5" mb={2} align="center">
+                                        Select Product
+                                    </Typography> */}
+                                    <FormControl fullWidth sx={{ maxWidth: '100%' }}>
+                                        <InputLabel id="product">Select Product</InputLabel>
+                                        <Select
+                                            labelId="product"
+                                            id="product"
+                                            name="product"
+                                            value={formData.product}
+                                            label="Select Product"
+                                            onChange={handleChange}
+                                        >
+                                            <MenuItem value=""> <em>None</em> </MenuItem>
+                                            <MenuItem value="1">Product 1</MenuItem>
+                                            <MenuItem value="2">Product 2</MenuItem>
+                                            <MenuItem value="3">Product 3</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                </Grid>
+                            )
+                        }
 
 
-                            <Grid item xs={12}>
-
-                            </Grid>
-                            <Grid item xs={12} sm={10} md={8}>
-                                <CustomCard>
-                                    <Typography variant="h5" mb={2} align="center">
-                                        Order Form
-                                    </Typography>
-
-                                    <TextField
-                                        fullWidth
-                                        label="Client Name"
-                                        name="clientName"
-                                        onChange={handleChange}
-                                        required
-                                        value={formData.clientName}
-                                        variant="outlined"
-                                        sx={{ mb: 2 }}
-                                    />
-                                    <TextField
-                                        fullWidth
-                                        label="Phone Number"
-                                        name="phoneNumber"
-                                        onChange={handleChange}
-                                        required
-                                        value={formData.phoneNumber}
-                                        variant="outlined"
-                                        sx={{ mb: 2 }}
-                                    />
-                                    <TextField
-                                        fullWidth
-                                        label="Email Address"
-                                        name="emailAddress"
-                                        onChange={handleChange}
-                                        required
-                                        value={formData.emailAddress}
-                                        variant="outlined"
-                                        sx={{ mb: 2 }}
-                                    />
-                                    <TextField
-                                        fullWidth
-                                        label="Address"
-                                        name="address"
-                                        onChange={handleChange}
-                                        required
-                                        value={formData.address}
-                                        variant="outlined"
-                                    />
-                                </CustomCard>
-                            </Grid>
+                        <Grid item xs={12}>
 
                         </Grid>
-                    </MotionInView>
-                </form>
-            </Box>
+                        <Grid item xs={12} sm={10} md={8}>
+                            <CustomCard>
+                                <Typography variant="h5" mb={2} align="center">
+                                    Order Form
+                                </Typography>
+
+                                <TextField
+                                    fullWidth
+                                    label="Client Name"
+                                    name="clientName"
+                                    onChange={handleChange}
+                                    required
+                                    value={formData.clientName}
+                                    variant="outlined"
+                                    sx={{ mb: 2 }}
+                                />
+                                <TextField
+                                    fullWidth
+                                    label="Phone Number"
+                                    name="phoneNumber"
+                                    onChange={handleChange}
+                                    required
+                                    value={formData.phoneNumber}
+                                    variant="outlined"
+                                    sx={{ mb: 2 }}
+                                />
+                                <TextField
+                                    fullWidth
+                                    label="Email Address"
+                                    name="emailAddress"
+                                    onChange={handleChange}
+                                    required
+                                    value={formData.emailAddress}
+                                    variant="outlined"
+                                    sx={{ mb: 2 }}
+                                />
+                                <TextField
+                                    fullWidth
+                                    label="Address"
+                                    name="address"
+                                    onChange={handleChange}
+                                    required
+                                    value={formData.address}
+                                    variant="outlined"
+                                />
+                            </CustomCard>
+                        </Grid>
+
+                    </Grid>
+                </MotionInView>
+            </form>
+        </Box>
     )
 }
