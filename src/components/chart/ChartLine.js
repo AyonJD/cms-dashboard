10accd7ef7df6c3ebfa536988ec6732f384cd11e
AyonJD <ayonjodder177@gmail.com>
2023-07-20 02:10:55 +0600

    Demo one pick hour

--- src/components/chart/ChartLine.js
+++ src/components/chart/ChartLine.js
@@ -1,26 +1,48 @@
-import { merge } from 'lodash';
-import React, { useEffect, useState } from 'react';
-import dynamic from 'next/dynamic';
-import BaseOptionChart from './BaseOptionChart';
+import { merge } from 'lodash'
+import React, { useEffect, useState } from 'react'
+import dynamic from 'next/dynamic'
+import BaseOptionChart from './BaseOptionChart'
 
-const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
+const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false })
 
-const CHART_DATA = [{ name: 'Peak Hours', data: [0, 1, 2, 8, 4, 5] }];
+const CHART_DATA = [{ name: 'Peak Hours', data: [10, 12, 8, 20, 14, 8] }]
 
 export default function ChartLine() {
-    const [isBrowser, setIsBrowser] = useState(false);
+  const [isBrowser, setIsBrowser] = useState(false)
 
-    useEffect(() => {
-        setIsBrowser(true);
-    }, []);
+  useEffect(() => {
+    setIsBrowser(true)
+  }, [])
 
-    const chartOptions = merge(BaseOptionChart(), {
-        xaxis: {
-            categories: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  const chartOptions = merge(BaseOptionChart(), {
+    xaxis: {
+      categories: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yaxis: {
+      min: 8,
+      max: 22,
+      tickAmount: 15,
+      labels: {
+        formatter: function (value) {
+          const hour = Math.floor(value)
+          const period = hour >= 12 ? 'PM' : 'AM'
+          const formattedHour = hour % 12 === 0 ? 12 : hour % 12
+          const formattedMinute = '00'
+          const formattedTime = `${formattedHour
+            .toString()
+            .padStart(2, '0')}:${formattedMinute} ${period}`
+          return formattedTime
         },
-    });
+      },
+    },
+  })
 
-    return isBrowser ? (
-        <ReactApexChart type="line" series={CHART_DATA} options={chartOptions} height={320} />
-    ) : null;
+  return isBrowser ? (
+    <ReactApexChart
+      type="line"
+      series={CHART_DATA}
+      options={chartOptions}
+      height={400}
+    />
+  ) : null
 }
